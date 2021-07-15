6567f6333e0c88c4d150ff53fa60d362c2bd2314
Daniel Caria <daniel.slack2@gmail.com>
2021-07-15 01:46:00 -0300

    inclusão de rotas para categorias

--- app/tables/tables.py
+++ app/tables/tables.py
@@ -4,10 +4,11 @@ from sqlalchemy.sql.sqltypes import Integer
 
 from app import db
 
+
 class Usuario(db.Model):
     __tablename__ = 'usuario'
-    __table_args__ = {"schema":"pos"}
-    
+    __table_args__ = {"schema": "pos"}
+
     id = db.Column(Integer, primary_key=True)
     public_id = db.Column(String(50), unique=True)
     nome = db.Column(String(100))
@@ -15,46 +16,48 @@ class Usuario(db.Model):
     senha = db.Column(String)
     admin = db.Column(Boolean)
 
-    
+
 class Unidade(db.Model):
     __tablename__ = 'unidade_medida'
     __table_args__ = {"schema": "pos"}
-    
+
     id = db.Column(Integer, primary_key=True)
     codigo = db.Column(String, nullable=False)
     descricao = db.Column(String, nullable=False)
-    
-    
+
+
 class Embalagem(db.Model):
     __tablename__ = 'embalagem'
     __table_args__ = {"schema": "pos"}
-    
+
     id = db.Column(Integer, primary_key=True)
     codigo = db.Column(String, nullable=False)
     descricao = db.Column(String, nullable=False)
-    
-    
+
+
 class Categoria(db.Model):
     __tablename__ = 'categoria'
-    __table_args__ = {"schema":"pos"}
-    
+    __table_args__ = {"schema": "pos"}
+
     id = db.Column(Integer, primary_key=True)
-    codigo = db.Column(String, nullable=False)
+    codigo = db.Column(String, nullable=False, unique=True)
     descricao = db.Column(String, nullable=False)
-    
+
+
 class SubCategoria(db.Model):
     __tablename__ = 'sub_categoria'
     __table_args__ = {"schema": "pos"}
-    
+
     id = db.Column(Integer, primary_key=True)
     codigo = db.Column(String, nullable=False)
     descricao = db.Column(String, nullable=False)
     categoria = db.Column(Integer, nullable=False)
 
+
 class Produto(db.Model):
     __tablename__ = 'produto'
-    __table_args__ = {"schema":"pos"}
-    
+    __table_args__ = {"schema": "pos"}
+
     id = db.Column(Integer, primary_key=True)
     codigo = db.Column(String(30), unique=True)
     nome = db.Column(String(100), nullable=False)
@@ -64,8 +67,8 @@ class Produto(db.Model):
     unidade = db.Column(Integer)
     embalagem = db.Column(Integer)
     categoria = db.Column(Integer)
-    
- 
+
+
 def create_table():
     db.drop_all()
-    db.create_all()   
+    db.create_all()
